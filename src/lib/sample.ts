1adf6fbdb3ed0a761bc0b3d384480b1f338db115
Craig Ostrin <craigostrin@gmail.com>
2023-10-11 12:05:16 +0200

    feat(sample puzzle)

---
+++ src/lib/sample.ts
@@ -0,0 +1,23 @@
+import { PuzzleData } from "./puzzle-data"
+
+export const sample: PuzzleData = {
+  author: "Craig",
+  puzzle: [
+    {
+      description: "Veggies",
+      answers: ["Broccoli", "Carrot", "Onion", "Celery"],
+    },
+    {
+      description: "Smash Bros Items",
+      answers: ["Mushroom", "Bat", "Hammer", "Ray Gun"],
+    },
+    {
+      description: "Nocturnal",
+      answers: ["Moth", "Owl", "Possum", "Racoon"],
+    },
+    {
+      description: "Cocktails",
+      answers: ["Screwdriver", "Bloody Mary", "Margarita", "Old Fashioned"],
+    },
+  ],
+}
